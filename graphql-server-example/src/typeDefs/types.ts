40f608ffa11f1e4f6a97d3d79cfb9003490b801a
gbayres <gbayres@hotmail.com>
2022-03-05 12:13:57 -0300

    Types

--- graphql-server-example/src/typeDefs/types.ts
+++ graphql-server-example/src/typeDefs/types.ts
@@ -14,10 +14,10 @@ export const types = gql`
 
 export interface PokemonInterface {
   pokedex_number: String,
-    name: String,
-    type1: String,
-    type2: String,
-    is_legendary: Boolean,
-    generation: String,
-    abilities: [String],
+  name: String,
+  type1: String,
+  type2: String,
+  is_legendary: Boolean,
+  generation: String,
+  abilities: [String],
 }
